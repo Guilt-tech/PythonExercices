41f625dbb36916be2e330f8a44ef8c0b1c389f05
Gabriel Verdi <gabrielverdi37@gmail.com>
2021-08-30 10:54:07 -0300

    Test

---
+++ Exercices/Secao05/exercicio01.py
@@ -0,0 +1,7 @@
+print('Digite dois números, para ver qual é o maior deles')
+n1 = int(input())
+n2 = int(input())
+if n1 > n2:
+    print(f'O primeiro número {n1} é maior que o segundo número {n2}')
+else:
+    print(f'O primeiro número {n2} é maior que o segundo número {n1}')
